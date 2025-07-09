c89591df5ef0b9df4c8642ea77d75c73c09583a5
GitHub Action <action@github.com>
2025-07-09 14:16:25 +0000

    Auto-increment minor version to 0.9.0 [skip ci]

--- alchimest.py
+++ alchimest.py
@@ -14,7 +14,7 @@ from rich.logging import RichHandler
 from rich.theme import Theme
 
 # Version information
-__version__ = "0.8.0"
+__version__ = "0.9.0"
 
 # Custom theme for log levels
 custom_theme = Theme(
