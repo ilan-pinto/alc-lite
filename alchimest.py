28ad58dc72f682a9c54dc27397783031d4e12540
GitHub Action <action@github.com>
2025-07-16 12:43:11 +0000

    Auto-increment minor version to 1.8.0 [skip ci]

--- alchimest.py
+++ alchimest.py
@@ -14,7 +14,7 @@ from rich.console import Console
 from rich.theme import Theme
 
 # Version information
-__version__ = "1.7.0"
+__version__ = "1.8.0"
 
 # Custom theme for log levels
 custom_theme = Theme(
